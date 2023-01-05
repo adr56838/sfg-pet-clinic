6fdf48136ad0e049289145df265cf28d28f42ce5
Adrian Malos <Malos.adrian@yahoo.com>
2023-01-05 20:51:13 +0200

    added Specialities, updated vets with speciality relationship. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Speciality> specialties;
 }
